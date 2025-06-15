7684fc151b89927deceabf2da0d58afa4aa9921c
bhaskar-hari-satyavolu <svbb577@gmail.com>
2025-06-15 12:15:05 +0530

    functions command

---
+++ functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VALIDATE(){
+
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR:: Installation failed"
+    else
+        echo "installation success"
+    fi
+}
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: run the commands with root user"
+else
+    echo "You are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
