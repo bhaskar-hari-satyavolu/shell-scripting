7c5010a4cbda96647a44d6689ee00ec17f667a3a
bhaskar-hari-satyavolu <svbb577@gmail.com>
2025-06-15 09:19:51 +0530

    username and passowrd

---
+++ username_password.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read -s USERNAME
+
+echo "please enter your passowrd"
+
+read -s PASSWORD
+
+echo "username is $USERNAME and passowrd is $PASSWORD"
