f56171d153479152e0db60101175f04f9a997744
bhaskar-hari-satyavolu <svbb577@gmail.com>
2025-06-15 11:58:24 +0530

    install mysql

---
+++ install_mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+$ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run with root access"
+    exit 1
+else
+    echo "You are root user"
+fi #fi means reverse of if, indicating the condition end
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing mysql is failed"
+else
+    echo "Installing mysql is success"
+fi
